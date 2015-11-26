deeeb7d43ba44aa081f4e6ee6276ad4140f06192
hieutrieu <mrhieutrieu@gmail.com>
2015-11-26 13:44:47 +0700

    aaa

---
+++ README.md
@@ -0,0 +1,30 @@
+# Flower Color Test
+
+## Description
+
+- Choose of 3 colors out of 8 colors.
+- Each color is represented as a flower-textured card.
+- Smartphone & PC compatible.
+- Fanpage: https://www.facebook.com/MegumiHarunaJewelry | ID 1441148306133126
+
+## Animation of Card
+
+- Before choice: Cards are flower-textured
+- After choice: Turned into gems + planets
+
+## The pages
+
+1. Introduction Page
+2. Choose Page
+    3 cards flippable.
+    After the choice of 3, next button is enabled on the bottom.
+3. Like Page
+    Show the like button. After user liked, show the result page.
+4. Result Page
+    Show Recommended Jewelry Pic on E-commerce site with the chosen card.
+    Show Share On Facebook.
+    
+## How to deploy the code
+
+1. Create facebook applications. Get the ID and place to the code.
+2. Submit for login review. To use user-likes permission.
---
+++ src/js/appConfig.js
@@ -0,0 +1,78 @@
+var appConfig = {
+    'jewelry': [
+        {
+            'name': 'jewelry name 1',
+            'desc': 'jewelry desc 1',
+            'link': 'http://google.com.vn'
+        },
+
+        {
+            'name': 'jewelry name 2',
+            'desc': 'jewelry desc 2',
+            'link': 'http://google.com.vn'
+        },
+
+        {
+            'name': 'jewelry name 3',
+            'desc': 'jewelry desc 3',
+            'link': 'http://google.com.vn'
+        }
+    ],
+    'planets': {
+        'budha': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Budha',
+            'desc': 'Budha Desc',
+            'content': "Budha Content"
+        },
+
+        'chandra': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Chandra',
+            'desc': 'Chandra Desc',
+            'content': "Chandra Content"
+        },
+
+        'guru': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Guru',
+            'desc': 'Guru Desc',
+            'content': "Guru Content"
+        },
+
+        'mangala': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Mangala',
+            'desc': 'Mangala Desc',
+            'content': "Mangala Content"
+        },
+
+        'rahu': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Rahu',
+            'desc': 'Rahu Desc',
+            'content': "Rahu Content"
+        },
+
+        'shani': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Shani',
+            'desc': 'Shani Desc',
+            'content': "Shani Content"
+        },
+
+        'shukra': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Shukra',
+            'desc': 'Shukra Desc',
+            'content': "Shukra Content"
+        },
+
+        'surya': {
+            'title': 'S明朝はニィスフォントのフラッグシップ明朝体です。W3からW9まで7種類の太',
+            'name': 'Surya',
+            'desc': 'Surya Desc',
+            'content': "Surya Content"
+        }
+    }
+};
